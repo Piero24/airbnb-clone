883d91217e46e2a1e8b728a59425034990770928
Piero24 <andrea_pietrobon@hotmail.it>
2024-08-03 23:29:52 +0200

    Completed Auth with MongoDB and Prisma. Completed login UI and login by email

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,10 @@ import "./globals.css";
 
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import LoginModal from "./components/modals/LoginModal";
 import RegisterModal from "./components/modals/RegisterModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import getCurrentUser from "./actions/getCurrentUser";  
 
 export const metadata: Metadata = {
   title: "Airbnb Clone",
@@ -16,18 +18,20 @@ const font = Nunito({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider/>
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser}/>
         </ClientOnly>
         {children}
       </body>
