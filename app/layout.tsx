014d59374f2bf242ae2f199dedd461bc50ac244d
Piero24 <andrea_pietrobon@hotmail.it>
2024-08-04 19:49:22 +0200

    Start Creating the listing for adding the properties

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
+
 import "./globals.css";
 
 import Navbar from "./components/navbar/Navbar";
 import ClientOnly from "./components/ClientOnly";
+import RentModal from "./components/modals/RentModal";
 import LoginModal from "./components/modals/LoginModal";
 import RegisterModal from "./components/modals/RegisterModal";
+
 import ToasterProvider from "./providers/ToasterProvider";
 import getCurrentUser from "./actions/getCurrentUser";  
 
@@ -29,6 +32,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider/>
+          <RentModal/>
           <LoginModal />
           <RegisterModal />
           <Navbar currentUser={currentUser}/>
