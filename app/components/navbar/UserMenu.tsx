2d0cf9ee7299a38a3037d528f87ad853249ace1e
Piero24 <andrea_pietrobon@hotmail.it>
2024-08-11 22:11:06 +0200

    Complete Favorites Page

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -107,7 +107,7 @@ const UserMenu: React.FC<UserMenuProps>= ({
                                     label="My Trips"
                                 />
                                 <MenuItem 
-                                    onClick={() => {}}
+                                    onClick={() => router.push('/favorites')}
                                     label="My Favorites"
                                 />
                                 <MenuItem 
